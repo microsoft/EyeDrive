137c26729163a802ec62fdb562795d28455ec282
Jay Beavers <jay.beavers@microsoft.com>
2018-02-08 13:22:53 -0800

    Add privacy url and turn on security code analysis

--- EyeDrive/Properties/AssemblyInfo.cs
+++ EyeDrive/Properties/AssemblyInfo.cs
@@ -1,10 +1,14 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("EyeDrive")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: AssemblyInformationalVersion("private")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
 
 [assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
